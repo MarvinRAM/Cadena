d6a64adafb08abae5071fef5f763f9eca1bc90f9
MarvinRAM <20243rd039@utez.edu.mx>
2025-05-08 12:03:28 -0600

    Ahora pide apellidos y muestra el nombre completo

--- Cadena.java
+++ Cadena.java
@@ -4,6 +4,8 @@ public class Cadena {
         Scanner almacenar = new Scanner(System.in);
         System.out.println("Escribe tu nombre");
         String nombre = almacenar.nextLine();
-        System.out.println("Tu nombre es: " + nombre);
+        System.out.println("Escribe tus apellidos");
+        String apellidos = almacenar.nextLine();
+        System.out.println("Tu nombre completo es: " + nombre + " " + apellidos);
     }
 }
